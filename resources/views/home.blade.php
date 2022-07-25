e8e3aa0fbb41abdae3d39f5aadd3f2590a64badb
Rahrahrarara <92094786+Rahrahrarara@users.noreply.github.com>
2022-07-25 17:39:37 +0100

    add my files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+   <div class="row">
+        <div class="col-3">
+            <img src="/freecodecampLogo.jpeg" alt="another logo" class="rounded-circle">
+        </div>
+
+        <div class="col-9">
+            side info here
+        </div>
+   </div>
+
+   <div class="row pt-4">
+    <div class="col-4">
+        <img src="https://images.unsplash.com/photo-1549407246-5b3d1fe7b9f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dGliZXRpYW5mbGFnc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="photo" class="w-100">
+    </div>
+    <div class="col-4">
+        <img src="https://images.unsplash.com/photo-1549407246-5b3d1fe7b9f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dGliZXRpYW5mbGFnc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="photo" class="w-100">
+    </div>
+    <div class="col-4">
+        <img src="https://images.unsplash.com/photo-1549407246-5b3d1fe7b9f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dGliZXRpYW5mbGFnc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="photo" class="w-100">
+    </div>
+
+   </div>
+</div>
+@endsection
